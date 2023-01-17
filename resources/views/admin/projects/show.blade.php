afdd38ea1f0c5e75d55cf43288e4d8f8c92aa4f8
karfaisarra <karfaisarra@gmail.com>
2023-01-17 16:47:15 +0100

    add modification to view show

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -1,20 +1,29 @@
 @extends('layouts.admin')
 @section('content')
-
-<div class="container py-5 d-flex gap-4">
-    @if($project->cover_image)
-    <img width="200" src="{{asset('storage/' .$project->cover_image)}}" alt="">
-    @else
-    <img width="200" src="https://picsum.photos/200/300" alt="">
-    @endif
-    <div class="details">
-        <h2>{{$project->title}}</h2>
-        <h5 class="pt-3">{{$project->slug}}</h5>
-        <strong>Type:</strong>
-        {{ $project->type ? $project->type->name : 'Untyped'}}
-        <p class="w-50 pt-3">{{$project->description}}</p>
+    <div class="container py-5 d-flex gap-4">
+        @if ($project->cover_image)
+            <img width="200" src="{{ asset('storage/' . $project->cover_image) }}" alt="">
+        @else
+            <img width="200" src="https://picsum.photos/200/300" alt="">
+        @endif
+        <div class="details">
+            <h2>{{ $project->title }}</h2>
+            <h5 class="pt-3">{{ $project->slug }}</h5>
+            <div>
+                <strong>Type:</strong>
+                {{ $project->type ? $project->type->name : 'Untyped' }}
+            </div>
+            <div>
+                <strong>Technologies:</strong>
+                @if (count($project->technologies) > 0)
+                    @foreach ($project->technologies as $technology)
+                        <span> {{ $technology->name }}/</span>
+                    @endforeach
+                @else
+                    <span>No Technologies associated to this Project!</span>
+                @endif
+            </div>
+            <p class="w-75 pt-3"><strong>Description: </strong>{{ $project->description }}</p>
+        </div>
     </div>
-</div>
-
-
 @endsection
